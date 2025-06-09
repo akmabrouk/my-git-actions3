4ce1dbe998d18835ade43f24c93785cda93ace3f
Abdel Mabrouk <aek.mabrouk@gmail.com>
2025-06-09 22:47:44 +0100

    update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Docker!');
+  res.send('Hello from Docker! This is to test ArgoCD');
 });
 
 app.listen(port, () => {
